9951285eef44b99a40e491e116000c3ac4ce6c61
Javier Caballero <javier.caballero@compass.com>
2025-09-12 09:09:57 -0300

    feat: enhance Vite configuration for dependency resolution and optimization

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['@emotion/react', '@emotion/styled'],
+  },
+  optimizeDeps: {
+    include: [
+      '@emotion/react',
+      '@emotion/styled',
+      '@mui/material',
+      '@mui/system',
+      '@mui/styled-engine'
+    ],
+  },
+  define: {
+    global: 'globalThis',
+  },
 })
