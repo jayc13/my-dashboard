8568d8ff7cdb59804d422346c1e57581e8d78090
Javier Caballero <caballerojavier13@gmail.com>
2025-09-20 23:30:00 -0300

    docs: Update docusaurus.config.ts

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://jayc13.github.io/my-dashboard',
+  url: 'https://jayc13.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/my-dashboard/',
