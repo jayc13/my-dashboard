6a6697ceadc987791f31157ed604d28e40edc570
Javier Caballero <caballerojavier13@gmail.com>
2025-09-22 17:43:00 -0300

    fix: Correct directory paths in railway build script for accurate navigation (#53)

--- scripts/railway-build.sh
+++ scripts/railway-build.sh
@@ -3,10 +3,10 @@
 # Start build process
 echo "\033[1;35m\033[1m\033[48;5;230m==========   S T A R T I N G   R A I L W A Y   B U I L D   ==========\033[0m"
 
-cd ../packages/types || exit
+cd ./packages/types || exit
 npm install --registry=https://registry.npmjs.org/ &>/dev/null
 npm run build &>/dev/null || { echo -e "\033[1;31mBuild failed.\033[0m"; exit 1; }
-cd ../server || exit
+cd ../../server || exit
 npm install --registry=https://registry.npmjs.org/ &>/dev/null
 npm run build &>/dev/null || { echo -e "\033[1;31mBuild failed.\033[0m"; exit 1; }
 
