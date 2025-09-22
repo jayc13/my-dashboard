acebadbe54da4614611c174d5c33ba0b5383051d
Javier Caballero <javier.caballero@compass.com>
2025-09-22 17:49:20 -0300

    fix: Update railway build script to ensure correct directory navigation

--- scripts/railway-build.sh
+++ scripts/railway-build.sh
@@ -3,8 +3,11 @@
 # Start build process
 echo "==========   S T A R T I N G   R A I L W A Y   B U I L D   =========="
 pwd
+cd ~ || exit
+pwd
 ls -al
-cd packages/types || exit
+cd packages/types
+ls -al
 npm install --registry=https://registry.npmjs.org/ &>/dev/null
 npm run build &>/dev/null || { echo -e "\033[1;31mBuild failed.\033[0m"; exit 1; }
 cd ~ || exit
