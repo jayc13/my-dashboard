9334ce22bfc9df5eec26abb9d17d3236fc557c17
Javier Caballero <javier.caballero@compass.com>
2025-09-22 18:25:21 -0300

    fix: Add package.json and package-lock.json for workspace configuration

--- scripts/railway-build.sh
+++ scripts/railway-build.sh
@@ -2,13 +2,12 @@
 
 # Start build process
 echo "==========   S T A R T I N G   R A I L W A Y   B U I L D   =========="
-cd packages/types || exit
-pwd
+
 npm install --registry=https://registry.npmjs.org/ #&>/dev/null
-npm run build # &>/dev/null || { echo -e "\033[1;31mBuild failed.\033[0m"; exit 1; }
-cd ../../server || exit
-pwd
-npm install --registry=https://registry.npmjs.org/ #&>/dev/null
-npm run build # &>/dev/null || { echo -e "\033[1;31mBuild failed.\033[0m"; exit 1; }
+
+npm run build --workspace=packages/types  # &>/dev/null || { echo -e "\033[1;31mBuild failed.\033[0m"; exit 1; }
+
+npm run build --workspace=server # &>/dev/null || { echo -e "\033[1;31mBuild failed.\033[0m"; exit 1; }
+
 # Deploy completed
 echo "✅  Build completed successfully!"
