cc51e32587e2e7592b2cc13d24e413c88f044561
Javier Caballero <javier.caballero@compass.com>
2025-09-24 15:32:14 -0300

    fix: Migrate project to pnpm

--- scripts/railway-server-build.sh
+++ scripts/railway-server-build.sh
@@ -3,12 +3,12 @@
 # Start build process
 echo "==========   S T A R T I N G   R A I L W A Y   B U I L D   =========="
 
-npm install --registry=https://registry.npmjs.org/ &>/dev/null
+npx pnpm install --registry=https://registry.npmjs.org/ &>/dev/null
 
-npm run build --workspace=packages/types &>/dev/null || { echo -e "\033[1;31mBuild Types failed.\033[0m"; exit 1; }
-npm run build --workspace=packages/sdk &>/dev/null || { echo -e "\033[1;31mBuild SDK failed.\033[0m"; exit 1; }
+npx pnpm run build --filter=packages/types &>/dev/null || { echo -e "\033[1;31mBuild Types failed.\033[0m"; exit 1; }
+npx pnpm run build --filter=packages/sdk &>/dev/null || { echo -e "\033[1;31mBuild SDK failed.\033[0m"; exit 1; }
 
-npm run build --workspace=server &>/dev/null || { echo -e "\033[1;31mBuild Server failed.\033[0m"; exit 1; }
+npx pnpm run build --filter=server &>/dev/null || { echo -e "\033[1;31mBuild Server failed.\033[0m"; exit 1; }
 
 # Deploy completed
 echo "✅  Build completed successfully!"
