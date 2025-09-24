6613a6b3b4a8a110817e56a5573696e44b85d0d3
Javier Caballero <javier.caballero@compass.com>
2025-09-24 16:04:17 -0300

    fix: Simplify build commands in railway build scripts

--- scripts/railway-cron-build.sh
+++ scripts/railway-cron-build.sh
@@ -3,9 +3,7 @@
 # Start build process
 echo "==========   S T A R T I N G   R A I L W A Y   B U I L D   =========="
 
-npx pnpm install --registry=https://registry.npmjs.org/ &>/dev/null
-
-npx pnpm run build --filter=cron &>/dev/null || { echo -e "\033[1;31mBuild Cron failed.\033[0m"; exit 1; }
+npx pnpm --filter=cron run build &>/dev/null || { echo -e "\033[1;31mBuild Cron failed.\033[0m"; exit 1; }
 
 # Deploy completed
 echo "✅  Build completed successfully!"
--- scripts/railway-server-build.sh
+++ scripts/railway-server-build.sh
@@ -3,12 +3,10 @@
 # Start build process
 echo "==========   S T A R T I N G   R A I L W A Y   B U I L D   =========="
 
-npx pnpm install --registry=https://registry.npmjs.org/ &>/dev/null
+npx pnpm --filter=@my-dashboard/types run build || { echo -e "\033[1;31mBuild Types failed.\033[0m"; exit 1; }
+npx pnpm --filter=@my-dashboard/sdk run build || { echo -e "\033[1;31mBuild SDK failed.\033[0m"; exit 1; }
 
-npx pnpm run build --filter=packages/types &>/dev/null || { echo -e "\033[1;31mBuild Types failed.\033[0m"; exit 1; }
-npx pnpm run build --filter=packages/sdk &>/dev/null || { echo -e "\033[1;31mBuild SDK failed.\033[0m"; exit 1; }
-
-npx pnpm run build --filter=server &>/dev/null || { echo -e "\033[1;31mBuild Server failed.\033[0m"; exit 1; }
+npx pnpm --filter=server run build || { echo -e "\033[1;31mBuild Server failed.\033[0m"; exit 1; }
 
 # Deploy completed
 echo "✅  Build completed successfully!"
