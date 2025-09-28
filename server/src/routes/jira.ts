72041e9746874e0a2e01e055a8bb1012fb083430
Javier Caballero <javier.caballero@compass.com>
2025-09-28 10:13:51 -0300

    refactor: Remove custom JIRA JQL endpoint and related functionality

--- server/src/routes/jira.ts
+++ server/src/routes/jira.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
-import { getJiraIssues, getManualQATasks, getMyTickets } from '../controllers/jira_controller';
+import { getManualQATasks, getMyTickets } from '../controllers/jira_controller';
 
 export function createJiraRouter() {
   const router = Router();
   // Define your Jira-related routes here
-  router.get('/', getJiraIssues);
   router.get('/manual_qa', getManualQATasks);
   router.get('/my_tickets', getMyTickets);
 
