1492212af9e99c5793cb6b13ab1e7b8d29309ded
Javier Caballero <javier.caballero@compass.com>
2025-10-05 17:37:13 -0300

    fix: Remove unnecessary console logs

--- client/src/sections/e2e-page/components/LoadingBackdrop.tsx
+++ client/src/sections/e2e-page/components/LoadingBackdrop.tsx
@@ -9,13 +9,6 @@ interface LoadingBackdropProps {
 const LoadingBackdrop = (props: LoadingBackdropProps) => {
   const { data, loading } = props;
 
-  console.log({
-    loading,
-    data: !!data,
-    status: data?.summary.status,
-    open: !loading && !!data && data?.summary.status === 'pending',
-  });
-
   const open = loading && !!data && data?.summary.status === 'pending';
 
   const message = data?.message || 'Loading...';
