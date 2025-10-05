db2d4cb063a59b6d14166daf103323f4ef184901
Javier Caballero <javier.caballero@compass.com>
2025-10-05 20:23:54 -0300

    feat: Update build process to include SDK dependencies

--- scripts/deployment/railway-cron-build.sh
+++ scripts/deployment/railway-cron-build.sh
@@ -4,6 +4,7 @@
 echo "==========   S T A R T I N G   R A I L W A Y   B U I L D   =========="
 
 pnpm --filter=@my-dashboard/types run build || { echo -e "\033[1;31mBuild Types failed.\033[0m"; exit 1; }
+pnpm --filter=@my-dashboard/sdk run build || { echo -e "\033[1;31mBuild SDK failed.\033[0m"; exit 1; }
 pnpm --filter=cron run build || { echo -e "\033[1;31mBuild Cron failed.\033[0m"; exit 1; }
 
 # Deploy completed
